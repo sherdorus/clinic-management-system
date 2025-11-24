57eae7c1ebb2cca6f1ee8ac95b49220c2f7fed0f
sherdorus <sherdor@ymail.com>
2025-11-25 00:17:09 +0500

    refactor(visits): temporarily remove createVisit to fix startup conflict
    - removed createVisit method from VisitController
    - kept getAllVisitors and findById functional
    - prepare for safe rewrite of createVisit with DTO mapping and service-layer logic

--- src/main/java/io/sherdor/clinicmanagementsystem/controller/VisitController.java
+++ src/main/java/io/sherdor/clinicmanagementsystem/controller/VisitController.java
@@ -6,9 +6,7 @@ import io.sherdor.clinicmanagementsystem.service.AppointmentService;
 import io.sherdor.clinicmanagementsystem.service.DoctorService;
 import io.sherdor.clinicmanagementsystem.service.PatientService;
 import io.sherdor.clinicmanagementsystem.service.VisitService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,14 +37,4 @@ public class VisitController {
         return ResponseEntity.ok(VisitDTO.fromEntity(visit));
     }
 
-    @PostMapping
-    public ResponseEntity<VisitDTO> createVisit(@Valid @RequestBody VisitDTO visitDTO) {
-        var patient = patientService.findById(visitDTO.getPatientId());
-        var doctor = doctorService.findById(visitDTO.getDoctorId());
-        var appointment = appointmentService.findById(visitDTO.getAppointmentId());
-        Visit visit = visitDTO.toEntity(patient, doctor, appointment);
-        Visit savedVisit = visitService.save(visit);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(VisitDTO.fromEntity(savedVisit));
-    }
 }
